dc73c46759b80e26d84ff77163c8b3903faabc34
IdanPipano <102733803+IdanPipano@users.noreply.github.com>
2022-06-27 22:30:03 +0300

    ,

--- IllegalReleaseAttempt.java
+++ IllegalReleaseAttempt.java
@@ -1,2 +1,10 @@
-public class IllegalReleaseAttempt {
+public class IllegalReleaseAttempt extends IllegalMonitorStateException{
+    String message;
+
+    public IllegalReleaseAttempt(){
+    }
+
+    public IllegalReleaseAttempt(String message){
+        this.message = message;
+    }
 }
