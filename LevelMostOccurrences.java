7a06bd99bd09d0ccabdb08471bb770e496e2c230
IdanPipano <102733803+IdanPipano@users.noreply.github.com>
2022-06-27 21:40:50 +0300

    ,

--- LevelMostOccurrences.java
+++ LevelMostOccurrences.java
@@ -3,8 +3,32 @@ import java.util.ArrayDeque;
 public class LevelMostOccurrences {
 
     public static int getLevelWithMostOccurrences(BinNode<Integer> node, int num) {
-        ArrayDeque<ArrayDeque<BinNode>> q = new ArrayDeque<>();
-        // TODO: Add your code here...
-        return 19;
+        int max = 0, rowNum = -1, occurrences;
+        BinNode<Integer> b;
+        ArrayDeque<BinNode<Integer>> parentsQueue = new ArrayDeque<>(), childrenQueue;
+        ArrayDeque<Integer> numOfOccurrencesInLevels = new ArrayDeque<>();
+        parentsQueue.addFirst(node);
+
+        while(!parentsQueue.isEmpty() ){
+            occurrences = 0;
+            childrenQueue = new ArrayDeque<>();
+            while (!parentsQueue.isEmpty()){
+                 b = parentsQueue.pollFirst();
+                 if (b.getData() == num) occurrences++;
+                 if (b.getRight() != null) childrenQueue.addLast(b.getRight());
+                 if (b.getLeft() != null) childrenQueue.addLast(b.getLeft());
+            }
+            parentsQueue = childrenQueue;
+            numOfOccurrencesInLevels.addLast(occurrences);
+        }
+
+        for (int i = 0; !numOfOccurrencesInLevels.isEmpty(); ++i) {
+            occurrences = numOfOccurrencesInLevels.pollFirst();
+            if (occurrences > max){
+                max = occurrences;
+                rowNum = i;
+            }
+        }
+        return rowNum;
     }
 }
